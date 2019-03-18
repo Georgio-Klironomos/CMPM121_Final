16b82afdd005bdcac664758f0874d363903b4ab6
Georgio Klironomos <gklirono@ucsc.edu>
2019-03-18 15:02:33 -0700

    Organization and Polish

---
+++ Great Artists Steal/Assets/Scripts/OuchScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OuchScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Ouch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ouch.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     private void OnTriggerEnter(Collider other) {
+         if(other.gameObject.CompareTag("Player")) {
+            Ouch.SetActive(true);
+         }
+     }
+}
